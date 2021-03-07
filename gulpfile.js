d53097392dd3f9d68c033704d0d87872c1423af5
mihmosi <mihmosi@gmail.com>
2021-03-07 23:15:30 +0300

    add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // const { src, dest } = require('gulp');
 
-let projectFolder = "dest";
+let projectFolder = "dist";
 let sourceFolder = "src";
 
 let path = {
@@ -12,24 +12,25 @@ let path = {
     fonts: projectFolder+"/fonts/",
   },
   src: {
-    html: sourceFolder+"/*.html",
+    html: [sourceFolder+"/*.html", "!"+sourceFolder+"/_*.html"],
     css: sourceFolder+"scss/style.scss",
     js: sourceFolder+"js/script.js",
-    img: sourceFolder+"/img/**/*.(jpg,png,svg,gif,ico,webp)",
+    img: sourceFolder+"/img/**/*.{jpg,png,svg,gif,ico,webp}",
     fonts: sourceFolder+ "/fonts/*.ttf",
   },
   watch: {
     html: sourceFolder+"/**/*.html",
     css: sourceFolder+"scss/**/*.scss",
     js: sourceFolder+"js/**/*.js",
-    img: sourceFolder+"/img/**/*.(jpg,png,svg,gif,ico,webp)",
+    img: sourceFolder+"/img/**/*.{jpg,png,svg,gif,ico,webp}",
   },
   clean: "./" + projectFolder + "/"
 }
 
 let { src , dest } = require('gulp'),
   gulp = require('gulp'),
-  browsersync = require('browser-sync').create();
+  browsersync = require('browser-sync').create(),
+  fileinclude = require('gulp-file-include');
   
 function browserSync(params) {
   browsersync.init({
@@ -43,13 +44,17 @@ function browserSync(params) {
 
 function html() {
   return src(path.src.html)
+    .pipe(fileinclude())
     .pipe(dest(path.bild.html))
     .pipe(browsersync.stream())
 }
 
-let bild = gulp.series(html);
+function watchFiles(params) {
+  gulp.watch([path.watch.html],  html)
+}
 
-let watch = gulp.parallel(bild, browserSync);
+let bild = gulp.series(html);
+let watch = gulp.parallel(bild, watchFiles, browserSync);
 
 exports.html = html;
 exports.bild = bild;
